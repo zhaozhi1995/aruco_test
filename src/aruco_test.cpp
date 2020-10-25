287cc3ae32bf6a473ae8bfb977aad8474c524411
NieXun <826355055@qq.com>
2020-10-25 20:01:00 +0800

    add marker pose estimate
    Signed-off-by: NieXun <826355055@qq.com>

--- src/aruco_test.cpp
+++ src/aruco_test.cpp
@@ -1,5 +1,3 @@
-// aruco_code_detect.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//#include "pch.h"
 #include <iostream>  
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
@@ -14,6 +12,9 @@ using namespace cv;
 using namespace std;
 
 ros::Publisher imgmarkerShowPub;
+ros::Publisher markerposePub;
+cv::Mat cameraMatrix;
+cv::Mat distCoeffs;
 
 void imageHandler(const sensor_msgs::Image::ConstPtr& imageData) 
 {
@@ -35,22 +36,69 @@ void imageHandler(const sensor_msgs::Image::ConstPtr& imageData)
 	// if at least one marker detected
 	if (ids.size() > 0) 
 	{
-		ROS_INFO("find %lu aruco markers", ids.size());
+		ROS_DEBUG("find %lu aruco markers", ids.size());
 		cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+		sensor_msgs::ImagePtr marker_show_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", imageCopy).toImageMsg();
+		imgmarkerShowPub.publish(*marker_show_msg);
+
+        std::vector<cv::Vec3d> rvecs, tvecs;
+        cv::aruco::estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+        // draw axis for each marker
+        for(int i=0; i<ids.size(); i++)
+            cv::aruco::drawAxis(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+		sensor_msgs::ImagePtr marker_pose_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", imageCopy).toImageMsg();
+		markerposePub.publish(*marker_pose_msg);
+	} else {
+		ROS_DEBUG("No aruco marker be found!", ids.size());
+		sensor_msgs::ImagePtr marker_show_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+		imgmarkerShowPub.publish(*marker_show_msg);
 	}
-
-	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", imageCopy).toImageMsg();
-	imgmarkerShowPub.publish(*msg);
-
 }
+
+void readParameters(std::string config_file)
+{
+    FILE *fh = fopen(config_file.c_str(),"r");
+    if(fh == NULL){
+        ROS_WARN("config_file dosen't exist; wrong config_file path");
+        ROS_BREAK();
+        return;          
+    }
+    fclose(fh);
+
+    cv::FileStorage fsSettings(config_file, cv::FileStorage::READ);
+    if(!fsSettings.isOpened())
+    {
+        std::cerr << "ERROR: Wrong path to settings" << std::endl;
+    }
+
+    fsSettings["cameraMatrix"] >> cameraMatrix;
+    fsSettings["distCoeffs"] >> distCoeffs;
+
+    fsSettings.release();
+}
+
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "aruco_marker_dect");
 	ros::NodeHandle nh;
 
+    if(argc != 2)
+    {
+        printf("please intput: rosrun aruco_test aruco_test [config file] \n"
+               "for example: rosrun aruco_test aruco_test "
+               "~/catkin_ws/src/aruco_test/config/camera.yaml \n");
+        return 1;
+    }
+
+    string camera_conf = argv[1];
+    ROS_INFO("camera_conf: %s", argv[1]);
+
+    readParameters(camera_conf);
+
 	ros::Subscriber imageSub = nh.subscribe<sensor_msgs::Image>("/image/raw", 1, imageHandler);
 	imgmarkerShowPub = nh.advertise<sensor_msgs::Image>("/image/marker_show", 1);
+	markerposePub = nh.advertise<sensor_msgs::Image>("/image/marker_pose_estimate", 1);
 
 	ros::spin();
     return 0;
